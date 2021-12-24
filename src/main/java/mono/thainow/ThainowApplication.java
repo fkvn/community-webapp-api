b9362871b7e54e7606240450c8f50e3d3c5200d9
Kevin Ngo <phucaone@gmail.com>
2021-12-23 22:51:55 -0800

    Initial commit

---
+++ src/main/java/mono/thainow/ThainowApplication.java
@@ -0,0 +1,20 @@
+package mono.thainow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+// for starter dependency when haven't configured database yet
+@EnableAutoConfiguration(
+exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, MongoAutoConfiguration.class})
+public class ThainowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThainowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mono/thainow/ThainowApplicationTests.java
@@ -0,0 +1,13 @@
+package mono.thainow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThainowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
