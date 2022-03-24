b1191dea2de0c692f8f85ffa69041b35c8231f15
Kevin Ngo <phucaone@gmail.com>
2022-03-23 19:37:24 -0700

    modified Location domain

--- src/main/java/mono/thainow/domain/location/Location.java
+++ src/main/java/mono/thainow/domain/location/Location.java
@@ -6,6 +6,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import lombok.EqualsAndHashCode;
@@ -20,53 +22,55 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 @Entity
+@Table(indexes = { @Index(name = "location_placeid_UNIQUE", columnList = "LOCATION_PLACEID", unique = true), })
 public class Location implements Serializable {
 	/**
 	* 
 	*/
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue
 	@Column(name = "LOCATION_ID")
 	private Long id;
-	
+
+	@Column(name = "LOCATION_PLACEID")
+	private Long placeid;
+
 	@Column(name = "LOCATION_ADDRESS1")
 	private String address1;
-	
+
 	@Column(name = "LOCATION_ADDRESS2")
 	private String address2;
-	
-	
+
 	@Column(name = "LOCATION_NEIGHBERHOOD")
 	private String neighberhood;
-	
+
 	@Column(name = "LOCATION_CITY")
+	@NotNull(message = "Location city can't be null")
 	private String locality;
-	
+
 	@Column(name = "LOCATION_COUNTY")
 	private String county;
-	
+
+	@NotNull(message = "Location state can't be null")
 	@Column(name = "LOCATION_STATE")
 	private String state;
-	
+
 	@Column(name = "LOCATION_COUNTRY")
 	private String country = "USA";
-	
+
 	@NotNull(message = "Location zipcode can't be null")
 	@Column(name = "LOCATION_ZIPCODE")
 	private String zipcode;
-	
+
 	@Column(name = "LOCATION_FORMATTED_ADDRESS")
 	private String formattedAddress;
-	
+
 	@Column(name = "LOCATION_LAT")
 	private String lat;
-	
+
 	@Column(name = "LOCATION_LNG")
 	private String lng;
-	
-	
+
 }
-
-
