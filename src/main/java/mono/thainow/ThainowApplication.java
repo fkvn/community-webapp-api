afa2f85309315c0b1c5242d6ba48e358be8a60e4
Kevin Ngo <phucaone@gmail.com>
2022-04-13 15:00:31 -0700

    configured and disabled authenticated for swagger

--- src/main/java/mono/thainow/ThainowApplication.java
+++ src/main/java/mono/thainow/ThainowApplication.java
@@ -2,33 +2,16 @@ package mono.thainow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 // for starter dependency when haven't configured database yet
 //@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
 ////		HibernateJpaAutoConfiguration.class, 
 //		MongoAutoConfiguration.class })
-@Configuration
-@EnableSwagger2
 public class ThainowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThainowApplication.class, args);
 	}
 	
-	@Bean 
-	public Docket productApi() {
-
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("mono.thainow.rest.controller"))
-				.paths(PathSelectors.any()).build();
-	}
 }
--- src/main/java/mono/thainow/configuration/WebSecurityConfig.java
+++ src/main/java/mono/thainow/configuration/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -51,45 +52,54 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //					}
 //				});
 //	}
-	
+
 // ------------- jwt ------------
 	@Autowired
 	UserDetailsServiceImpl userDetailsServiceImpl;
-	
+
 	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
-	
+
 	@Bean
 	public AuthTokenFilter authenticationJwtTokenFilter() {
 		return new AuthTokenFilter();
 	}
-	
+
 	@Override
 	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 		authenticationManagerBuilder.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
 	}
-	
+
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable()
-			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-			.antMatchers("/api/**").permitAll()
-			.anyRequest().authenticated();;
-			
-		http.addFilterBefore(authenticationJwtTokenFilter(), (Class<? extends Filter>) UsernamePasswordAuthenticationFilter.class);
+		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/api/auth/**").permitAll().antMatchers("/api/**").permitAll().anyRequest()
+				.authenticated();
+		;
+
+		http.addFilterBefore(authenticationJwtTokenFilter(),
+				(Class<? extends Filter>) UsernamePasswordAuthenticationFilter.class);
+	}
+
+	
+	// ignore swagger
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+		web.ignoring().mvcMatchers("/swagger-ui/**", "/swagger-ui.html/**", "/configuration/**",
+				"/swagger-resources/**", "/v2/api-docs");
 	}
 
 }
