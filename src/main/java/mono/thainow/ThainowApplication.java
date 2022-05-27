d37ab0f873dd692a5a04ffffe605cb82f0f23adb
Kevin Ngo <phucaone@gmail.com>
2022-05-26 20:57:24 -0700

    setting Elasticsearch

--- src/main/java/mono/thainow/ThainowApplication.java
+++ src/main/java/mono/thainow/ThainowApplication.java
@@ -1,7 +1,13 @@
 package mono.thainow;
 
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.settings.Settings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 // for starter dependency when haven't configured database yet
@@ -14,4 +20,20 @@ public class ThainowApplication {
 		SpringApplication.run(ThainowApplication.class, args);
 	}
 	
+	@Bean
+	public boolean createTestIndex(RestHighLevelClient restHighLevelClient) throws Exception {
+		try {
+			DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("hello-world");
+			restHighLevelClient.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT); // 1
+		} catch (Exception ignored) {
+		}
+
+		CreateIndexRequest createIndexRequest = new CreateIndexRequest("hello-world");
+		createIndexRequest.settings(
+				Settings.builder().put("index.number_of_shards", 1)
+						.put("index.number_of_replicas", 0));
+		restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT); // 2
+
+		return true;
+	}
 }
