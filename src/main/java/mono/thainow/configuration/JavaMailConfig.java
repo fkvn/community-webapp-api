60b6676405f34861b353c59fee22939bec018ce0
Kevin Ngo <phucaone@gmail.com>
2022-11-06 13:05:34 -0800

    Create JavaMailConfig.java

---
+++ src/main/java/mono/thainow/configuration/JavaMailConfig.java
@@ -0,0 +1,16 @@
+package mono.thainow.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailConfig {
+	
+	
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
